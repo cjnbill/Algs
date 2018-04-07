027d1499d266e755e70f28ac5ada3ae674a92b46
cjnbill <cjnseu@gmail.com>
2018-04-06 20:46:01 -0400

    remove source

---
+++ src/cmake-build-debug/CMakeFiles/algs.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/cjn/ClionProjects/algs/src/LongestSubwithK.cpp" "/Users/cjn/ClionProjects/algs/src/cmake-build-debug/CMakeFiles/algs.dir/LongestSubwithK.cpp.o"
+  "/Users/cjn/ClionProjects/algs/src/Trie.cpp" "/Users/cjn/ClionProjects/algs/src/cmake-build-debug/CMakeFiles/algs.dir/Trie.cpp.o"
+  "/Users/cjn/ClionProjects/algs/src/main.cpp" "/Users/cjn/ClionProjects/algs/src/cmake-build-debug/CMakeFiles/algs.dir/main.cpp.o"
+  "/Users/cjn/ClionProjects/algs/src/union_find.cpp" "/Users/cjn/ClionProjects/algs/src/cmake-build-debug/CMakeFiles/algs.dir/union_find.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/cmake-build-debug/CMakeFiles/algs.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algs.dir/main.cpp.o"
+  "CMakeFiles/algs.dir/LongestSubwithK.cpp.o"
+  "CMakeFiles/algs.dir/union_find.cpp.o"
+  "CMakeFiles/algs.dir/Trie.cpp.o"
+  "algs.pdb"
+  "algs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
