6a11811316c229c3f5a6ff0766a899996e8aab98
cjnbill <cjnseu@gmail.com>
2018-05-15 20:38:57 -0400

    distance

--- src/Graph/BFS/BfsProblems.cc
+++ src/Graph/BFS/BfsProblems.cc
@@ -50,30 +50,7 @@ public:
 
 };
 
-//542. 01 Matrix
-//add 0 to queue first,then do bfs to update the shortest distance to 1
-static vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-    int m=matrix.size(),n=matrix[0].size();
-    vector<pair<int,int>> dirs={{0,1},{1,0},{0,-1},{-1,0}};
-    queue<pair<int,int>> q;
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(matrix[i][j]==0)
-                q.push({i,j});
-            else matrix[i][j]=INT_MAX;
-        }
-    }
-    while(!q.empty()){
-        auto t=q.front();q.pop();
-        for(auto dir:dirs){
-            int x=t.first+dir.first,y=t.second+dir.second;
-            if(x<0||x>=m||y<0||y>=n||matrix[x][y]<=matrix[t.first][t.second]+1)continue;
-            matrix[x][y]=matrix[t.first][t.second]+1;
-            q.push({x,y});
-        }
-    }
-    return matrix;
-}
+
 
 
 //207. course schedule
@@ -149,6 +126,80 @@ static pair<int,int> BikePerson(vector<vector<int>>& grid, pair<int,int> person)
     }
     //return pos[person.first][person.second];
     return {};
+}
+
+/*
+ * 距离场
+ *
+*/
+
+//542. 01 Matrix, walls and gates
+//find nearest 0 for each 1
+//距离场初始化为 0 for target,INT_MAX for source
+//add 0 to queue first,then do bfs to update the shortest distance to 1
+static vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+    int m=matrix.size(),n=matrix[0].size();
+    vector<pair<int,int>> dirs={{0,1},{1,0},{0,-1},{-1,0}};
+    queue<pair<int,int>> q;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(matrix[i][j]==0)
+                q.push({i,j});
+            else matrix[i][j]=INT_MAX;
+        }
+    }
+    while(!q.empty()){
+        auto t=q.front();q.pop();
+        for(auto dir:dirs){
+            int x=t.first+dir.first,y=t.second+dir.second;
+            if(x<0||x>=m||y<0||y>=n||matrix[x][y]<=matrix[t.first][t.second]+1)continue;
+            matrix[x][y]=matrix[t.first][t.second]+1;
+            q.push({x,y});
+        }
+    }
+    return matrix;
+}
+
+//317. Shortest Distance from All Buildings
+static int shortestDistance(vector<vector<int>>& grid) {
+    int res = INT_MAX, buildingCnt = 0, m = grid.size(), n = grid[0].size();
+    vector<vector<int>> dist(m, vector<int>(n, 0)), cnt = dist;
+    vector<vector<int>> dirs{{0,-1},{-1,0},{0,1},{1,0}};
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (grid[i][j] == 1) {
+                ++buildingCnt;
+                queue<pair<int, int>> q;
+                q.push({i, j});
+                vector<vector<bool>> visited(m, vector<bool>(n, false));
+                int level = 1;
+                while (!q.empty()) {
+                    int size = q.size();
+                    for (int s = 0; s < size; ++s) {
+                        int a = q.front().first, b = q.front().second; q.pop();
+                        for (int k = 0; k < dirs.size(); ++k) {
+                            int x = a + dirs[k][0], y = b + dirs[k][1];
+                            if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 0 && !visited[x][y]) {
+                                dist[x][y] += level;
+                                ++cnt[x][y];
+                                visited[x][y] = true;
+                                q.push({x, y});
+                            }
+                        }
+                    }
+                    ++level;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (grid[i][j] == 0 && cnt[i][j] == buildingCnt) {
+                res = min(res, dist[i][j]);
+            }
+        }
+    }
+    return res == INT_MAX ? -1 : res;
 }
 
 
